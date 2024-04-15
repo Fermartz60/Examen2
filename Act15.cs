c2ffd9269a2f5eae4932a43eec7a0d90a759b2a2
Fermartz60 <119077073+Fermartz60@users.noreply.github.com>
2024-04-15 11:32:42 -0600

    Add files via upload

---
+++ Act15.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Data
+{
+    public class Persona
+    {
+        [Required(ErrorMessage = "El número de inventario es Requerido")]
+        [MaxLength(100, ErrorMessage = "Debe ser un valor positivo y único Valido")]
+        public string? Inventario { get; set; }
+
+        [Required(ErrorMessage = "Requiere nombre de Producto")]
+        [MaxLength(10, ErrorMessage = "Máximo 10 letras")]
+        public string? Nombre { get; set; }
+
+        public enum Departamento
+        {
+            Frutas,
+            Verduras,
+            Lacteos
+        }
+        public Departamento SelectedDepartamento { get; set; }
+    }
+}
